3f1435005a6ffcbe4cdff00a4cb341d291ca6b9c
azhikcodes <codesazhik@gmail.com>
2024-05-17 11:55:29 +0300

    first commit

---
+++ mylidege2/settings.py
@@ -0,0 +1,127 @@
+
+import os
+from pathlib import Path
+import dj_database_url
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+SECRET_KEY = 'django-insecure-dw8*zd(+^ut3fl*&bkq8l2#*mfus+5i5shg1!l(i$7@1p(!5^6'
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+
+INSTALLED_APPS = [
+    'applicantsReg',
+    'jazzmin',
+    'import_export',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
+]
+
+
+ROOT_URLCONF = 'mylidege2.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mylidege2.wsgi.application'
+
+
+
+
+DATABASES = {
+       'default':{
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'lidegeDatabase',
+          'USER': 'lidegeAdmin',
+          'PASSWORD': 'Azaria9961',
+          'HOST': 'database-1.cju8m4u40w5e.eu-north-1.rds.amazonaws.com',  
+          'PORT': '5432', 
+      }
+   }
+
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets') 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'applicantsReg.CustomUser'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+
+
+
+
